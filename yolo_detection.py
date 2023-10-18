8e43d4956d8164896495ec96d3a61d036dd6f178
vyacheslav.nesterov <nesterov_kotelnikovo@mail.ru>
2023-10-18 17:26:21 +0300

    Configure logger

--- yolo_detection.py
+++ yolo_detection.py
@@ -1,7 +1,29 @@
+import argparse
+import logging
+import cv2 as cv
+import pandas as pd
 from ultralytics import YOLO
 
+LOG_FORMAT = '%(asctime)s   [%(levelname)s] %(funcName)s  %(lineno)d: %(message)s'
+DATE_FORMAT = '%H:%M:%S'
+LOGGER = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO, format=LOG_FORMAT, datefmt=DATE_FORMAT)
+
 def detect_peoples(video_path, show_results, to_csv_path):
-    pass
+    model = YOLO('data/models/yolov8l.pt')
+    yolo_df = video_processing(model, video_path)
+
+
+def video_processing(yolo, video_path):
+    cap = cv.VideoCapture(video_path)
+    if not cap.isOpened():
+        LOGGER.critical("Video Capture is not opened")
+
 
 if __name__ == '__main__':
-    detect_peoples('video/50sec.mp4', True, 'data/detections.csv')
+    parser = argparse.ArgumentParser(description="Mp4 yolo detection")
+    parser.add_argument("video_path", type=str, help="Path to video file")
+    parser.add_argument("show_results", type=bool, help="Will detection results be shown or not")
+    parser.add_argument("to_csv_path", type=str, help="Path to csv file which will be consist detection results")
+    args = parser.parse_args()
+    detect_peoples(args.video_path, args.show_results, args.to_csv_path)
