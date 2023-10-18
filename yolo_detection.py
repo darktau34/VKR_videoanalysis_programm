71d6356fa1fd09aecfca4d2aeea10e4e94bee6fe
vyacheslav.nesterov <nesterov_kotelnikovo@mail.ru>
2023-10-18 16:48:44 +0300

    Init repository

---
+++ yolo_detection.py
@@ -0,0 +1,7 @@
+from ultralytics import YOLO
+
+def detect_peoples(video_path, show_results, to_csv_path):
+    pass
+
+if __name__ == '__main__':
+    detect_peoples('video/50sec.mp4', True, 'data/detections.csv')
