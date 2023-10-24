8c15e4f74b305a5fa54afe597608ded30de625c9
vyacheslav.nesterov <nesterov_kotelnikovo@mail.ru>
2023-10-24 23:31:53 +0300

    Added clipping video into fragments

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import torch
 import pandas as pd
 
 from yolo_detection import detect_peoples
-from photobox import save_photoboxes_from_yolo
+from videoprocessing import save_photoboxes_from_yolo
+from videoprocessing import clip_video_fragment
 
 LOG_FORMAT = '%(asctime)s   [%(levelname)s] %(name)s -- %(funcName)s  %(lineno)d: %(message)s'
 DATE_FORMAT = '%H:%M:%S'
@@ -18,6 +19,7 @@ def create_data_dirs(video_path):
     file = os.path.splitext(file)
     filename = file[0]
 
+    # data dir
     data_dir = 'data/' + filename
     to_csv_path = data_dir + '/' + filename + '.csv'
     if not os.path.isdir(data_dir):
@@ -26,6 +28,7 @@ def create_data_dirs(video_path):
     else:
         LOGGER.info('Directory %s already exists', data_dir)
 
+    # photoboxes dir
     photoboxes_dir = data_dir + '/' + 'photoboxes'
     if not os.path.isdir(photoboxes_dir):
         os.makedirs(photoboxes_dir)
@@ -35,7 +38,17 @@ def create_data_dirs(video_path):
 
     photoboxes_dir += '/'
 
-    return to_csv_path, photoboxes_dir
+    # videoclips dir
+    videoclips_dir = data_dir + '/' + 'videoclips'
+    if not os.path.isdir(videoclips_dir):
+        os.makedirs(videoclips_dir)
+        LOGGER.info('Directory %s was created', videoclips_dir)
+    else:
+        LOGGER.info('Directory %s already exists', videoclips_dir)
+
+    videoclips_dir += '/'
+
+    return to_csv_path, photoboxes_dir, videoclips_dir
 
 
 def main():
@@ -46,13 +59,20 @@ def main():
     parser = argparse.ArgumentParser(description="Mp4 yolo detection")
     parser.add_argument("video_path", type=str, help="Path to video file")
     parser.add_argument("--show_results", action='store_true', default=False, help="Detection results be shown or not")
+    parser.add_argument("--clip_sec", type=int, default=5, help="Number of seconds for walking video fragment")
     args = parser.parse_args()
 
-    to_csv_path, photoboxes_dir = create_data_dirs(args.video_path)
+    video_path = args.video_path
+    show_results = args.show_results
+    max_clip_seconds = min(args.clip_sec, 15)
 
-    detect_peoples(args.video_path, args.show_results, to_csv_path)
+    to_csv_path, photoboxes_dir, videoclips_dir = create_data_dirs(video_path)
+
+    detect_peoples(video_path, show_results, to_csv_path)
     yolo_df = pd.read_csv(to_csv_path)
-    save_photoboxes_from_yolo(args.video_path, yolo_df, photoboxes_dir)
+    save_photoboxes_from_yolo(video_path, yolo_df, photoboxes_dir)
+
+    clip_video_fragment(video_path, yolo_df, videoclips_dir, max_clip_seconds)
 
 
 if __name__ == '__main__':
---
+++ videoprocessing.py
@@ -0,0 +1,113 @@
+import logging
+import numpy as np
+from moviepy.editor import VideoFileClip, ImageSequenceClip
+from PIL import Image
+
+logger = logging.getLogger(__name__)
+
+
+def save_photoboxes_from_yolo(video_path, yolo_df, dir_to_save):
+    video = VideoFileClip(video_path)
+    person_arr = yolo_df.tracker_id.unique().astype(int)
+    logger.info('Persons number: %s', len(person_arr))
+    for person in person_arr:
+        only_person_df = yolo_df.loc[yolo_df.tracker_id == person]
+        mean_frame = only_person_df.frame.values.mean()
+        mean_frame = mean_frame.round().astype(int)
+        frame_row = only_person_df.loc[only_person_df.frame == mean_frame]
+        x1 = frame_row.x1.values[0]
+        y1 = frame_row.y1.values[0]
+        x2 = frame_row.x2.values[0]
+        y2 = frame_row.y2.values[0]
+        box = (x1, y1, x2, y2)
+        photobox = cut_photobox(video, mean_frame, box)
+        photobox.save(dir_to_save + 'person' + str(person) + '.png')
+
+    logger.info('Photoboxes are saved to path: %s', dir_to_save)
+
+
+def cut_photobox(video_clip, frame_number, box):
+    fps = video_clip.fps
+    frame_sec = (1 / fps) * frame_number
+    frame = video_clip.get_frame(frame_sec)
+    pil_im = Image.fromarray(frame)
+    photobox = pil_im.crop(box)
+    return photobox
+
+
+def clip_video_fragment(video_path, yolo_df, dir_to_save, max_clip_seconds):
+    video = VideoFileClip(video_path)
+    fps = video.fps
+    max_clip_frames = int(max_clip_seconds * fps)
+    person_arr = yolo_df.tracker_id.unique().astype(int)
+    logger.info('Video fps: %s', str(fps))
+    logger.info('Video fragment in seconds: %s', str(max_clip_seconds))
+    logger.info('Video fragment in frames: %s', str(max_clip_frames))
+    for person in person_arr:
+        only_person_df = yolo_df.loc[yolo_df.tracker_id == person]
+        only_person_df.reset_index(drop=True, inplace=True)
+        print(only_person_df)
+        video_clip_df = only_person_df[:max_clip_frames]
+        print(video_clip_df)
+
+        max_width = 0.0
+        max_height = 0.0
+        for index, row in video_clip_df.iterrows():
+            x1 = row['x1']
+            y1 = row['y1']
+            x2 = row['x2']
+            y2 = row['y2']
+            cur_width = x2 - x1
+            cur_height = y2 - y1
+            if cur_width > max_width:
+                max_width = cur_width
+            if cur_height > max_height:
+                max_height = cur_height
+
+        max_width = round(max_width)
+        max_height = round(max_height)
+        # print(f'W: {max_width} \t H: {max_height}')
+        images = []
+        for index, row in video_clip_df.iterrows():
+            frame_num = int(row['frame'])
+            x1 = row['x1']
+            y1 = row['y1']
+            x2 = row['x2']
+            y2 = row['y2']
+            box = (x1, y1, x2, y2)
+            box = increase_box(box, max_width, max_height)
+            img = cut_photobox(video, frame_num, box)  # ошибка т.к. это возвращает pil img
+            # print(img)
+            img = np.asarray(img)
+            images.append(img)
+        clip = ImageSequenceClip(images, fps=fps)
+        # clip.write_videofile('test' + str(person) + '.mp4', fps=fps)
+        clip.write_gif(dir_to_save + 'person' + str(person) + '.gif', fps=fps, program='ffmpeg')
+        if person == 13:
+            break
+
+
+def increase_box(box, max_width, max_height):
+    box_width = box[2] - box[0]
+    box_height = box[3] - box[1]
+    if box_width != max_width:
+        diff = max_width - box_width
+        diff = diff / 2
+        x1 = box[0] - diff
+        x2 = box[2] + diff
+    else:
+        x1 = box[0]
+        x2 = box[2]
+
+    if box_height != max_height:
+        diff = max_height - box_height
+        diff = diff / 2
+        y1 = box[1] - diff
+        y2 = box[3] + diff
+    else:
+        y1 = box[1]
+        y2 = box[2]
+
+    new_box = (x1, y1, x2, y2)
+
+    return new_box
